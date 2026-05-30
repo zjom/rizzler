6a393f757e191a4674119eb489029eef36ecd27d
Zihan Jin <admin@zihanjin.com>
2026-05-31 01:08:30 +1000

    shut up clippy

--- src/keymap/mod.rs
+++ src/keymap/mod.rs
@@ -2,5 +2,6 @@ mod default;
 mod event;
 mod registry;
 mod trie;
+#[allow(unused_imports)]
 pub use event::{KeyCode, KeyEvent, KeyModifiers};
 pub use registry::KeymapRegistry;
--- src/mode.rs
+++ src/mode.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum EditingMode {
     Insert,
